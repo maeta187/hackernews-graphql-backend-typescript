32553cd9b5493fad23183c6cae15b4ae6804f5d3
maeta187 <nexus18789@gmail.com>
2023-03-05 20:34:51 +0900

    🏷️  ; 型定義ファイルの作成

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+import { type } from 'os'
+
+export interface MyContext {
+  // Context typing
+  prisma: PrismaClient
+}
+
+export type SignupType = {
+  email: string
+  password: string
+  name: string
+}
